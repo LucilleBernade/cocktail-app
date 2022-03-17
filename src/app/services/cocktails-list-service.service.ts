99de9de038c1f88b86888821a1ecaf71b8b8880c
LucilleBernade <bernadelucille@gmail.com>
2022-03-17 16:47:31 +0100

    mise à jour

--- src/app/services/cocktails-list-service.service.ts
+++ src/app/services/cocktails-list-service.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { Cocktails } from '../models/cocktails';
+import { Drink } from '../models/drink.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,16 @@ export class CocktailsListServiceService {
         let url="https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
         return this.http.get<Cocktails>(url);
     }
+
+    getDrinks(param=""): Observable<Drink[]>
+  {
+    return this.getCocktailList().pipe(
+      //tap(cocktail=>console.log("tap1", cocktail)),
+      map(({drinks})=> drinks.filter(drink=>drink.strDrink.includes(param))),
+      tap(drinks=>console.log("tap3", drinks)),
+     )
+  }
+
 }
 
 //getCocktailList(): Observable<{"drinks":Array<Drink>}>
